8cb6915765932d31640ae90615c8346480c337ba
Benedikt Schnatterbeck <benediktms@hey.com>
2023-01-12 09:02:54 +0000

    style: add prettier
    style: add prettier ignore file
    
    style: apply formatting
    
    style: install and apply svelte prettier

--- src/routes/App/lib/Footer.svelte
+++ src/routes/App/lib/Footer.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-    export let footerText: string;
+  export let footerText: string;
 </script>
 
 <p class="footerText">{footerText}</p>
 
 <style>
-    .footerText {
-        text-align: center;
-        font-size: 12px;
-        font-weight: 500;
-        color: var(--secondary-text-color);
-        font-family: Helvetica;
-        font-style: normal;
-        margin-top: 8px;
-    }
+  .footerText {
+    text-align: center;
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--secondary-text-color);
+    font-family: Helvetica;
+    font-style: normal;
+    margin-top: 8px;
+  }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,12 +15,12 @@ export default defineConfig({
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
     // Tauri supports es2021
-    target: ["es2021", "chrome100", "safari13"],
+    target: ['es2021', 'chrome100', 'safari13'],
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
