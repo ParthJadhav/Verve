9c47633ffa436b148822020d5d1e2e89c9f57a6e
Benedikt Schnatterbeck <benediktms@hey.com>
2023-01-14 08:53:01 +0000

    fix: hight styling

--- src/routes/App/App.svelte
+++ src/routes/App/App.svelte
@@ -97,7 +97,8 @@
 
 <style>
   .container {
-    height: 350px;
+    height: 100%;
+    max-height: 350px;
     width: 750px;
     background-color: var(--primary-bg-color);
     border-radius: 10px;
